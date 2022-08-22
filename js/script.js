df7ccdeaac5af492357b1066c11864bbe97a9751
katibur <katiburrahmansany@gmail.com>
2022-08-22 15:59:35 +0600

    validation added for every input field

--- js/script.js
+++ js/script.js
@@ -11,3 +11,53 @@ playerName('nasir-name', 'nasir-select-btn');
 playerName('rubel-name', 'rubel-select-btn');
 playerName('mustafiz-name', 'mustafiz-select-btn');
 playerName('taskin-name', 'taskin-select-btn');
+
+
+// calculate the expense for per player
+document.getElementById('calculate').addEventListener('click', function () {
+
+    const perPlayerExpenseInputField = document.getElementById('per-player');
+
+    console.log(perPlayerExpenseInputField.value);
+
+    // validation of per player expense input field
+    if (perPlayerExpenseInputField.value == '' || perPlayerExpenseInputField.value < 0) {
+        alert('Invalid Amount/Type');
+        perPlayerExpenseInputField.value = '';
+        return;
+    }
+
+    // find the number of players added in the list and calculate expense for them
+    let orderderList = document.querySelectorAll("li");
+
+    let order = parseInt(orderderList.length);
+
+    const playerExpenseShown = document.getElementById('palyer-expenses');
+
+    const perPlayerExpense = parseFloat(perPlayerExpenseInputField.value);
+    const playerExpense = perPlayerExpense * (order - 5);
+
+    playerExpenseShown.innerText = parseFloat(playerExpense.toFixed('2'));
+})
+
+// calculate the total amount
+document.getElementById('calculate-total').addEventListener('click', function () {
+
+    const playerExpenses = document.getElementById('palyer-expenses');
+    const managerExpenses = document.getElementById('manager');
+    const coachExpenses = document.getElementById('coach');
+
+    const totalCost = document.getElementById('total');
+
+    // validation for manager,coach input field and calculate the total expense
+    if (playerExpenses.innerText == '' || managerExpenses.value == '' || managerExpenses.value < 0 || coachExpenses.value == '' || coachExpenses.value < 0) {
+        alert('invalid Amount/Type');
+        managerExpenses.value = '';
+        coachExpenses.value = '';
+        return;
+    }
+
+    const total = parseFloat(playerExpenses.innerText) + parseFloat(managerExpenses.value) + parseFloat(coachExpenses.value);
+
+    totalCost.innerText = parseFloat(total);
+})
--- js/utilities.js
+++ js/utilities.js
@@ -24,7 +24,9 @@ function playerName(playerNameId, playerButton) {
         let orderderList = document.querySelectorAll("li");
 
         let order = parseInt(orderderList.length);
-        if (order > 5) {
+
+        // length considered 10 because by default 5 listed previously
+        if (order > 10) {
             alert('can not add more');
             disableButton.disabled = false;
             disableButton.style.background = 'blue';
@@ -34,63 +36,3 @@ function playerName(playerNameId, playerButton) {
         }
     })
 }
-
-// validation for per-player expense's input field
-function perPlayerExpense() {
-    const perPlayerExpenseInput = document.getElementById('per-player');
-    if (perPlayerExpenseInput.value < 0) {
-        alert('Budget For A Player Can Not Be Lower Than Zero(0)');
-        perPlayerExpenseInput.value = '';
-        return;
-    }
-}
-
-document.getElementById('calculate').addEventListener('click', function () {
-
-    const perPlayerExpenseInputField = document.getElementById('per-player');
-
-    let orderderList = document.querySelectorAll("li");
-
-    let order = parseInt(orderderList.length);
-
-    const playerExpenseShown = document.getElementById('palyer-expenses');
-
-    const perPlayerExpense = parseFloat(perPlayerExpenseInputField.value);
-    const playerExpense = perPlayerExpense * order;
-
-    playerExpenseShown.innerText = parseFloat(playerExpense.toFixed('2'));
-})
-
-// validation for manager expense's input field
-function managerExpense() {
-    const managerExpenseInput = document.getElementById('manager');
-    if (managerExpenseInput.value < 0) {
-        alert('Budget For A Manager Can Not Be Lower Than Zero(0)');
-        managerExpenseInput.value = '';
-        return;
-    }
-}
-
-// validation for coach expense's input field
-function coachExpense() {
-    const coachExpenseInput = document.getElementById('coach');
-    if (coachExpenseInput.value < 0) {
-        alert('Budget For A Coach Can Not Be Lower Than Zero(0)');
-        coachExpenseInput.value = '';
-        return;
-    }
-}
-
-// calculate the total amount
-document.getElementById('calculate-total').addEventListener('click', function () {
-
-    const playerExpenses = document.getElementById('palyer-expenses');
-    const managerExpenses = document.getElementById('manager');
-    const coachExpenses = document.getElementById('coach');
-
-    const totalCost = document.getElementById('total');
-
-    const total = parseFloat(playerExpenses.innerText) + parseFloat(managerExpenses.value) + parseFloat(coachExpenses.value);
-
-    totalCost.innerText = parseFloat(total);
-})
